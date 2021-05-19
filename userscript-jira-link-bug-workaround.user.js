e727def93eede23fc007f60582cc59b5cd34ee86
feulix <felix@vangeuns.nl>
2021-05-19 10:44:10 +0200

    Update userscript-jira-link-bug-workaround.user.js

--- userscript-jira-link-bug-workaround.user.js
+++ userscript-jira-link-bug-workaround.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Jira board link bug workaround
-// @version  1.1.0
+// @version  1.1.1
 // @updateURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @downloadURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @grant    none
@@ -9,27 +9,25 @@
 // ==/UserScript==
 
 
-docReady(function() {
-    replaceLinks();
-});
-              
+
+// alternative to DOMContentLoaded
+document.onreadystatechange = function () {
+    console.log('document.readyState',document.readyState)
+    if (document.readyState == "interactive") {
+  	    replaceLinks();
+	}
+    else if (document.readyState == "complete") {
+        replaceLinks();
+    }
+}
+
 function replaceLinks() {
-  
+
   console.log('User Script is replacing links');
   var anchors = document.getElementsByTagName("a");
 
   for (var i = 0; i < anchors.length; i++) {
       anchors[i].href = anchors[i].href.replace( "useStoredSettings=true", "useStoredSettings=false" );
   }
-  
-}
 
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}    
+}         
