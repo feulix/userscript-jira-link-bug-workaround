d9306b3a5f29478022c25b33ae93ae8d5b587e1d
feulix <felix@vangeuns.nl>
2021-05-19 13:26:13 +0200

    Update userscript-jira-link-bug-workaround.user.js

--- userscript-jira-link-bug-workaround.user.js
+++ userscript-jira-link-bug-workaround.user.js
@@ -5,18 +5,18 @@
 // @downloadURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @grant    none
 // @include  *.atlassian.net/*
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 
 
 // alternative to DOMContentLoaded
 document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-  	    replaceLinks();
-	}
-    else if (document.readyState == "complete") {
-        replaceLinks();
+    console.log('document.readyState',document.readyState)
+    if (document.readyState == "interactive" || document.readyState == "complete") {
+//	      AJS.$(window).load(function($) {
+	        replaceLinks();
+//        })
     }
 }
 
@@ -24,6 +24,7 @@ function replaceLinks() {
 
   console.log('User Script is replacing links');
   var anchors = document.getElementsByTagName("a");
+
   for (var i = 0; i < anchors.length; i++) {
       anchors[i].href = anchors[i].href.replace( "useStoredSettings=true", "useStoredSettings=false" );
   }
