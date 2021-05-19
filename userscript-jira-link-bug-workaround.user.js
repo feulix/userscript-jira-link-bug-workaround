6f3404fb0e444a1ac71affca7bfa73b3dd8a925a
feulix <felix@vangeuns.nl>
2021-05-19 10:37:47 +0200

    Added support for Safari

--- userscript-jira-link-bug-workaround.user.js
+++ userscript-jira-link-bug-workaround.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Jira board link bug workaround
-// @version  1.0.0
+// @version  1.1.0
 // @updateURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @downloadURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @grant    none
@@ -9,15 +9,12 @@
 // ==/UserScript==
 
 
-// alternative to DOMContentLoaded
-document.onreadystatechange = function () {
-  if (document.readyState == "interactive") {
-  	replaceLinks();
-	}
-}
+docReady(function() {
+    replaceLinks();
+});
               
 function replaceLinks() {
-
+  
   console.log('User Script is replacing links');
   var anchors = document.getElementsByTagName("a");
 
@@ -26,3 +23,13 @@ function replaceLinks() {
   }
   
 }
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
