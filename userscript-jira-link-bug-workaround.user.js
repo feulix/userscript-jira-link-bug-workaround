0425a22a72aa1e2d078934f88cf7a55ae9844cbb
feulix <felix@vangeuns.nl>
2021-05-24 14:59:26 +0200

    Updated eventlisteners

--- userscript-jira-link-bug-workaround.user.js
+++ userscript-jira-link-bug-workaround.user.js
@@ -1,32 +1,37 @@
 // ==UserScript==
 // @name     Jira board link bug workaround
-// @version  1.1.2
+// @version  2.0
 // @updateURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @downloadURL	https://github.com/feulix/userscript-jira-link-bug-workaround/raw/main/userscript-jira-link-bug-workaround.user.js
 // @grant    none
 // @include  *.atlassian.net/*
-// @run-at       document-end
 // ==/UserScript==
 
 
 
-// alternative to DOMContentLoaded
-document.onreadystatechange = function () {
-    console.log('document.readyState',document.readyState)
-    if (document.readyState == "interactive" || document.readyState == "complete") {
-//	      AJS.$(window).load(function($) {
-	        replaceLinks();
-//        })
+(function() {
+
+  
+  var replaceLinks = function() {
+
+  	console.log('User Script is replacing links');
+	  var anchors = document.getElementsByTagName("a");
+
+    for (var i = 0; i < anchors.length; i++) {
+      anchors[i].href = anchors[i].href.replace("useStoredSettings=true", "useStoredSettings=false");
     }
-}
-
-function replaceLinks() {
-
-  console.log('User Script is replacing links');
-  var anchors = document.getElementsByTagName("a");
-
-  for (var i = 0; i < anchors.length; i++) {
-      anchors[i].href = anchors[i].href.replace( "useStoredSettings=true", "useStoredSettings=false" );
   }
+  
+  window.addEventListener("load",function(event) {
+    replaceLinks();
+  },false);
+  
+  window.addEventListener("readystatechange",function(event) {
+    replaceLinks();
+  },false);  
 
-}         
+  window.addEventListener("DOMContentLoaded",function(event) {
+    replaceLinks();
+  },false);
+
+})();
